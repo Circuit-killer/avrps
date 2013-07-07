d5feb047ec544e18f2c5d31d503435475af03ce5
Martin Panter <vadmium  gmailcom>
2013-07-07 03:46:59 +0000

    Allow button to switch it back on while it is still powered up

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <stdint.h>
 #include <avr/wdt.h>
 
+static void switch_on(void);
+
 /* ADC2: 1/2 battery voltage */
 #define BAT_ADC 2
 #define BAT_BIT ADC2_BIT
@@ -16,19 +18,13 @@ FUSES = {
 };
 #define F_CPU (9600UL * 1000 / 8)
 
-static uint8_t flash_time = 25;
+static uint8_t flash_time;
 static uint8_t button_time = 0;
 
+static enum {MODE_ON, MODE_SHUTDOWN, MODE_OFF} mode;
+
 int main(void) {
-    /* Set waveform generation mode (WGM) to fast PWM, with TOP = OCR0A
-    ("variable") */
-    OCR0A = 24 - 1; /* F_CPU / 50 kHz - 1 */
-    OCR0B = 20 - 1; /* 84.5% * 24 */
-    TCNT0 = 0;
-    REG_MOD(TCCR0A, WGM0A_MASK << WGM0A_BIT | COM_MASK << COM0B0,
-        WGM0A_PWM_FAST << WGM0A_BIT | COM_PWM_NONINV << COM0B0);
-    REG_MOD(TCCR0B, 1 << WGM02 | CS_MASK << CS00,
-        WGM02_PWM_VARIABLE << WGM02 | CS_CLK << CS00);
+    switch_on();
     
     DDRB = MASK_MERGE(~0 * DD_INPUT, 1 << OC0B_BIT | 1 << 2, ~0 * DD_OUTPUT);
     
@@ -56,7 +52,7 @@ int main(void) {
 }
 
 ISR(WDT_vect) {
-    if(flash_time && 0 == --flash_time) {
+    if(mode == MODE_ON && 0 == --flash_time) {
         if(OCR0B <= 13 - 1) {
             OCR0B = 20 - 1;
             flash_time = 2;
@@ -67,15 +63,51 @@ ISR(WDT_vect) {
     }
     
     if(button_time && 0 == --button_time) {
-        OCR0B = 6 - 1;
-        flash_time = 0;
+        switch(mode) {
+        case MODE_ON:
+            OCR0B = 6 - 1;
+            mode = MODE_SHUTDOWN;
+            break;
+        case MODE_SHUTDOWN:
+            OCR0B = 5 - 1;
+            mode = MODE_OFF;
+            break;
+        case MODE_OFF:
+            switch_on();
+            break;
+        }
     }
 }
 
 ISR(ANA_COMP_vect) {
-    if(!bit_is_set(ACSR, ACO)) {
-        button_time = 80;
-    } else {
-        button_time = 0;
+    switch(mode) {
+    case MODE_ON:
+        if(0 == (ACSR >> ACO & 1)) {
+            button_time = 80;
+        } else {
+            button_time = 0;
+        }
+        break;
+    case MODE_SHUTDOWN:
+    case MODE_OFF:
+        if(1 == (ACSR >> ACO & 1)) {
+            button_time = 3;
+        } else {
+            button_time = 0;
+        }
+        break;
     }
+}
+
+static void switch_on(void) {
+    /* Set waveform generation mode (WGM) to fast PWM, with TOP = OCR0A
+    ("variable") */
+    OCR0A = 24 - 1; /* F_CPU / 50 kHz - 1 */
+    OCR0B = 20 - 1; /* 84.5% * 24 */
+    TCNT0 = 0;
+    TCCR0A = WGM0A_PWM_FAST << WGM0A_BIT | COM_PWM_NONINV << COM0B0;
+    TCCR0B = WGM02_PWM_VARIABLE << WGM02 | CS_CLK << CS00;
+    
+    mode = MODE_ON;
+    flash_time = 25;
 }
